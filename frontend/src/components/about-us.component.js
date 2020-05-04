910e3a5e9777446a1b4f203fd8f7b30ee1e402b8
gilbert-khonstantine <khonstantine@gmail.com>
2020-05-04 17:06:50 +0800

    add about us

--- frontend/src/components/about-us.component.js
+++ frontend/src/components/about-us.component.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 export default class aboutUsPage extends Component {
     render() {
         return (
-            <h1> ABOUT US</h1 >
+            <div className="container">
+                <h1> ABOUT US</h1 >
+                <p>Homelessness: A Global Issue</p>
+                <p>Homelessness is a prime concern for numerous countries across the globe, including the most advanced ones. Though the population of homeless people tends to be higher in developing countries, it is still a concern in developed nations. Homelessness is a state where someone lacks regular shelter. The term includes people who do not have a home and sleep on the streets, as well as those whose basic nighttime residence is a homeless shelter or other improvised form of housing. Such people lack the capacity to acquire or maintain sufficient nighttime shelter. Individuals and family units become homeless for many reasons. Assessing these causes can help to mitigate the issue.</p>
+                <p>According to a report issued in 2005 by the United Nations Commission on Human Rights, approximately 100 million individuals are homeless globally (Bywaters, Eileen, and Napier 150). This population has no shelter and must sleep in doorways, on sidewalks, under bridges, in incomplete buildings, and in parks. They also resort to places such as public buildings and bus and railway stations. Night shelters are also significant, as they offer a roof over their heads.</p>
+            </div >
         )
     }
 }
